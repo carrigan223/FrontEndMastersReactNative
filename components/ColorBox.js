6b84cfa2d1766d660b2de47fc9ebd9ff351b4621
Andrew Carrigan <carrigan223@gmail.com>
2020-10-05 13:02:51 -0700

    Flatlist exercise

--- components/ColorBox.js
+++ components/ColorBox.js
@@ -6,9 +6,22 @@ const ColorBox = (props) => {
   const colorStyle = {
     backgroundColor: props.hexCode,
   };
+
+  //textColor has the property color deciding on black orwhite
+  //for the text depending on the hexcode of the background color
+  //we are using parseint() turn the hexcode into an interger and compare the contrast
+  //against the setting of 1.1 to decide if displayed in black or
+  //white, if divided by a higher number the result is more likely to be black
+  const textColor = {
+    color:
+      parseInt(props.hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
+  };
+
   return (
     <View style={[styles.box, colorStyle]}>
-      <Text style={styles.text}>
+      <Text style={[styles.text, textColor]}>
         {props.colorName}
         {props.hexCode}
       </Text>
@@ -19,7 +32,6 @@ const ColorBox = (props) => {
 const styles = StyleSheet.create({
   text: {
     fontWeight: 'bold',
-    color: 'white',
   },
   box: {
     padding: 10,
